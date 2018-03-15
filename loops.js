fc736cdbf8f2b948a21e7b3a3c66fee3ff1a423c
ana.rajner@gmail.com <ana.rajner@gmail.com>
2018-03-15 14:33:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,6 @@
 function forLoop(array) {
   for (var i=0; i<25;1++) {
-    if (i===1){
-      array.push(`I am ${i} strange loop.`);
+    array.push(`I am ${i} strange loop$(i===1 ? '' : 's').`);
     }
-    else {
-      array.push(`I am ${i} strange loops.`);
-    }
-  }
   return array;
 }
